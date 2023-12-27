4cb2af5c7c490b0eafd10232594fc559a04f3572
Sampad S Mohanty <sampad.swarupp@gmail.com>
2023-12-27 14:13:49 +0530

    Update base64_converter.py

--- app/sf_analytics_file_uploader/src/base64_converter.py
+++ app/sf_analytics_file_uploader/src/base64_converter.py
@@ -1,14 +1,32 @@
 import json
 import base64
 
-def json_to_base64(json_file_path):
-    with open(json_file_path) as jsonfile:
+def json_to_base64(json_file_path: str) -> str:
+    """
+    Convert JSON data from a file to base64 encoding.
+
+    Parameters:
+    - json_file_path (str): Path to the JSON file.
+
+    Returns:
+    - str: Base64-encoded string.
+    """
+    with open(json_file_path, 'r') as jsonfile:
         data = json.load(jsonfile)
     datastr = json.dumps(data)
     base64_encoded = base64.b64encode(datastr.encode('utf-8')).decode('utf-8')
     return base64_encoded
 
-def csv_to_base64(csv_file_path):
+def csv_to_base64(csv_file_path: str) -> str:
+    """
+    Convert CSV data from a file to base64 encoding.
+
+    Parameters:
+    - csv_file_path (str): Path to the CSV file.
+
+    Returns:
+    - str: Base64-encoded string.
+    """
     with open(csv_file_path, 'r') as file:
         csv_data = file.read()
     csv_bytes = csv_data.encode('utf-8')
